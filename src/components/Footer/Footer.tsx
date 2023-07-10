0c5ce0ed28dea5f1a5d415af02366e1b4f32fe03
Antonio Hauren <antoniohauren@protonmail.com>
2023-07-10 09:35:10 -0300

    feat: add footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,59 @@
+import { footerLinks } from "@/constants/footerLinks";
+import Image from "next/image";
+import Link from "next/link";
 import { FooterProps } from ".";
 
 export function Footer({}: FooterProps) {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            className="object-contain"
+            width={118}
+            height={18}
+          />
+
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+
+              {link.links.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.url}
+                  className="text-gray-500"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2023 CarHub. All Rights Reverved</p>
+
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 }
